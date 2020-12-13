35828e5bee4a69b852e1fe8c829efc5988a4f34a
Bruno Orlandi <brorlandi@gmail.com>
2020-12-13 14:44:48 -0300

    Add sounds and gifs.

--- index.js
+++ index.js
@@ -1,93 +1,74 @@
 /* Config */
-const twitchTvHandle = "bdougieYO";
-const PAUSE_DURATION = 30 * 1000; // 30 seconds
-const DISPLAY_DURATION = 10 * 1000; // 10 seconds
+const twitchTvHandle = 'OrlandiBot';
+const DEFAULT_DURATION = 10 * 1000; // 10 seconds
 
 /* DOM */
-const container = document.querySelector(".alerts");
-const img = new Image();
+const container = document.querySelector('.alerts');
 const queue = new Queue();
 
-/* Sound Effects */
-const pewAudio = new Audio("horn.wav");
-const magicChime = new Audio("Magic_Chime.mp3");
-
-/* GIFs */
-const beyGif = "https://media.giphy.com/media/VxkNDa92gcsRq/giphy.gif";
-const welcomeGif = "https://media.giphy.com/media/l3V0doGbp2EDaLHJC/giphy.gif";
-const pizzaGif = "https://media.giphy.com/media/3o6nUXaNE4wdhq8Foc/giphy.gif";
-
-
 // Resolve promise after duration
-const wait = async duration => {
-  return new Promise(resolve => setTimeout(resolve, duration));
+const wait = async (duration) => new Promise((resolve) => setTimeout(resolve, duration));
+
+// <h1 class="text-shadows">${user + generateTitle[type]}</h1>
+function gifAlert({ gif, audio, duration }) {
+  queue.add(async () => {
+    audio.play();
+    if (gif) {
+      container.innerHTML = `
+        <img src="${gif}" />
+      `;
+      container.style.opacity = 1;
+    }
+
+    await wait(duration * 1000 || DEFAULT_DURATION);
+
+    if (!queue.isLooping) {
+      container.style.opacity = 0;
+      container.innerHTML = '';
+      audio.pause();
+    }
+  });
+}
+
+const soundCommands = {
+  caixao: {
+    audio: new Audio('sons/caixao.mp3'),
+    gif: 'gifs/caixao.gif',
+    duration: 8,
+  },
+  errou: {
+    audio: new Audio('sons/errou.mp3'),
+    gif: 'gifs/errou.gif',
+    duration: 1.45,
+  },
+  sad: {
+    audio: new Audio('sons/sad.mp3'),
+  },
+  alert: {
+    audio: new Audio('sons/alert.mp3'),
+  },
+  jaaviseiquevaidarmerda: {
+    audio: new Audio('sons/jaaviseiquevaidarmerda.mp3'),
+    gif: 'gifs/jaaviseiquevaidarmerda.gif',
+    duration: 2,
+  },
 };
 
-const pauseSpotify = () => {
-  fetch("https://serve.onegraph.com/graphql?app_id=cdf2ebe1-3ad3-408a-81c0-1ed675d76411", {body: '{"doc_id": "10fccd15-1a55-4a27-877a-a63106b4bd11"}', method: "POST"})
-}
+const playSoundCommand = (soundCommandConfig) => {
+  new gifAlert(soundCommandConfig);
+};
 
 ComfyJS.Init(twitchTvHandle);
 ComfyJS.onCommand = (user, command, message, flags, extra) => {
   console.log(`!${command} was typed in chat`);
 
-  if (command == "yo") {
-    new gifAlert(user, beyGif, pewAudio, command);
-  }
+  const soundCommandConfig = soundCommands[command];
 
-  if (command == "welcome") {
-    new gifAlert(message, welcomeGif, magicChime, command);
-  }
-  // Ok, ready!
-  if(command == "music") {
-    new gifAlert(user, beyGif, pewAudio, command);
-    // Please don't stop the music
-    fetch("https://serve.onegraph.com/graphql?app_id=cdf2ebe1-3ad3-408a-81c0-1ed675d76411", {body: '{"doc_id": "e5e25f29-7862-4f23-8f53-8fb4373a0672"}', method: "POST"})
-
-    const EVADE_THE_DMCA_BAN_LENGTH = 2500;
-    setTimeout(() => {
-      // Well, stop it after 2.5 seconds...
-      // Pause the player
-      pauseSpotify();
-    }, EVADE_THE_DMCA_BAN_LENGTH)
-  }
-
-  if (flags.broadcaster && command == "pizza") {
-    new gifAlert(message, pizzaGif, magicChime, command);
-  }
-
-  if (flags.broadcaster && command == "pause") {
-    // Clear GIF queue and pause for PAUSE_DURATION
-    queue.clear();
-    queue.pause(PAUSE_DURATION);
+  if (soundCommandConfig) {
+    playSoundCommand(soundCommandConfig);
   }
 };
 
-ComfyJS.onChat = (user, message, flags, self, extra) => {
-  console.log(user + ":", message);
-};
-
-const generateTitle = {
-  yo: " is hype!",
-  welcome: " needs a welcome!",
-  pizza: " needed a pizza party!",
-  music: " stopped the music!"
-};
-
-function gifAlert(user, gif, audio, type) {
-  queue.add(async () => {
-    audio.play();
-    container.innerHTML = `
-      <h1 class="text-shadows">${user + generateTitle[type]}</h1>
-      <img src="${gif}" />
-    `;
-    container.style.opacity = 1;
-
-    await wait(DISPLAY_DURATION);
-
-    if (!queue.isLooping) {
-      container.style.opacity = 0;
-    }
-
-  });
-}
+// ComfyJS.onChat = (user, message, flags, self, extra) => {
+//   console.log(`${user}:`, message);
+// };
